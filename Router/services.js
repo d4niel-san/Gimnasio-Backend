c0bd57d8e3854b9ce37c2490406d4a961e7c06e3
d4niel-san <91570360+d4niel-san@users.noreply.github.com>
2022-03-27 12:54:14 -0300

    limpieza, conexion BBDD separada, router con funciones, intento de separar func de router (not work)

---
+++ Router/services.js
@@ -0,0 +1,48 @@
+//intente separar las funciones de las rutas... no tuve exito
+
+async function getUser(req, res) {
+    try {
+        const arrayUserDB = await User.find()
+        console.log(arrayUserDB)
+        res.json(arrayUserDB)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function logUser(req, res) {
+    const logUser = req.query
+    const email = logUser.email
+    const password = logUser.password
+    try {
+        const userDB = await User.findOne({ email })
+        if (userDB.password === password)
+            res.send(true)
+        else {
+            res.send(false)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+async function queryUser(req, res) {
+    try {
+        const userDB = new User(req.body)
+        await userDB.save()
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+function serverStart(req, res) {
+    res.send('Server escuchando en /')
+}
+
+module.exports = {
+    getUser: getUser,
+    logUser: logUser,
+    queryUser: queryUser,
+    serverStart: serverStart
+}
